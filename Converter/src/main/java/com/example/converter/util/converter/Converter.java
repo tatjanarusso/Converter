6e5adf584eb833461c93edd7e946a43ba6e6b3b3
janloeliger <jan.leliger@gmail.com>
2022-01-27 09:22:43 +0100

    parent class, added pdf libraries

--- Converter/src/main/java/com/example/converter/util/converter/Converter.java
+++ Converter/src/main/java/com/example/converter/util/converter/Converter.java
@@ -1,4 +1,16 @@
 package com.example.converter.util.converter;
 
+import java.nio.file.Path;
+
 public class Converter {
+    protected String pathToOriginal;
+    protected String pathToNew;
+
+    public Converter(String original, String newFile){
+        pathToOriginal = original;
+        pathToNew = newFile;
+    }
+    public boolean convert(){
+        return false;
+    }
 }
